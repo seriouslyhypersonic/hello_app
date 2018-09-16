cdc58ce5a9e80cde3e7e6151add7346c04906807
Nuno Alves de Sousa <nunoalvesdesousa@me.com>
2018-09-16 23:22:06 +0100

    Added remove student from classrom Prevented adding duplicates

--- classroom.hpp
+++ classroom.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include "cstdlib"
 
 #include "student.hpp"
 
@@ -13,6 +14,7 @@ class Classroom
 private:
     std::vector<Student*> students_;
     const std::string className_;
+
 public:
     Classroom(const std::string &className, Student* p_student = nullptr) : className_ {className}
     {
@@ -25,9 +27,38 @@ public:
     void add(Student* p_student)
     {
         if (p_student) {
+
+            // Detect if student was already added
+            if (in_classroom(p_student)) {
+                std::cerr << "error: " << p_student->name() << " already in the classrom\n";
+                return;
+            }
+
             students_.push_back(p_student);
             std::cout << "Added " << p_student->name() << " to " << className_ << '\n';
         }
+    }
+
+    void remove(Student* p_student)
+    {
+        for (auto it = students_.begin(); it != students_.end(); ) {
+            if (*it == p_student) {
+                std::cout << "Removing " << (*it)->name() << '\n';
+                students_.erase(it);
+            } else {
+                ++it;
+            }
+        }
+    }
+
+    bool in_classroom(Student *p_student)
+    {
+        for (const auto &p_aStudent : students_) {
+            if (p_aStudent == p_student) {
+                return true;
+            }
+        }
+        return false;
     }
 
     friend std::ostream& operator<<(std::ostream &out, const Classroom &classroom)
--- main.cpp
+++ main.cpp
@@ -13,8 +13,14 @@ int main(int argc, char* argv[])
     Classroom mathClass("Maths I", &john);
 
     Student anna("Anna");
+    Student ethan("Ethan");
     
     mathClass.add(&anna);
+    mathClass.add(&ethan);
+
+    std::cout << mathClass << '\n';
+
+    mathClass.remove(&anna);
 
     std::cout << mathClass << '\n';
 }
